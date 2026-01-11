afeff62c1c8264dfd0dbcc5d5d867217ca94a5f2
Thornchheun Sambo <thornchheunsambo@gmail.com>
2026-01-11 14:36:36 +0700

    fix

--- king_coffee/lib/main.dart
+++ king_coffee/lib/main.dart
@@ -10,7 +10,7 @@ class KINGCOFFEE extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demos',
       theme: ThemeData(
         // This is the theme of your application.
         //
